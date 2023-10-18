271214f736673d78874ade9d1ddd12989eb909a8
Rizki Budi <rizkibudi513@gmail.com>
2023-10-18 20:27:55 +0700

    feat: setup fonts and colors

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { useFonts } from "expo-font";
+import { Righteous_400Regular } from "@expo-google-fonts/righteous";
+import { Poppins_500Medium } from "@expo-google-fonts/poppins";
+import { IstokWeb_400Regular } from "@expo-google-fonts/istok-web";
+import { Inter_600SemiBold } from "@expo-google-fonts/inter";
 
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    Righteous_400Regular,
+    Poppins_500Medium,
+    IstokWeb_400Regular,
+    Inter_600SemiBold,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
 }
@@ -13,8 +30,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ app/themes/color.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  neutral: "#FFFFFF",
+  skyBlue: "#3CA2FF",
+  royalPurple: "#4C1FC7",
+  deepBlue: "#1167BD",
+  lightAzure: "#37CCFF",
+  darkIndigo: "#4839D2",
+  royalBlue: "#4449D5",
+  mediumBlue: "#3D98FB",
+};
